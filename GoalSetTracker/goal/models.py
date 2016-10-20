cf3b280c6d1591aaefa28ae925976d98f5f393de
francisco nievas <frannievas@gmail.com>
2016-10-20 12:14:08 -0300

    Nuevo modelo de metas y submetas

--- GoalSetTracker/goal/models.py
+++ GoalSetTracker/goal/models.py
@@ -36,6 +36,7 @@ class AbstractGoal(models.Model):
 class Goal(AbstractGoal):
     categoria = models.CharField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.categoria
 
