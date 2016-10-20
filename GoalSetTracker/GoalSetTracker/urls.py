c0eee428ef72fed04807ee2e0bbd840b98022ed2
francisco nievas <frannievas@gmail.com>
2016-10-20 00:20:36 -0300

    Modificar metas listo.

--- GoalSetTracker/GoalSetTracker/urls.py
+++ GoalSetTracker/GoalSetTracker/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     url(r'^home/(?P<username>[\w.@+-]+)/$', login_views.home, name='home'),
     url(r'^home/$', login_views.home, name='home'),
     url(r'^new_goal$', goal_views.new_goal, name='new_goal'),
-    url(r'^new_sub_goal$', goal_views.new_sub_goal, name='new_sub_goal'),
+    # url(r'^new_sub_goal$', goal_views.new_sub_goal, name='new_sub_goal'),
+    # url(r'^modify_goal$', goal_views.modify_goal, name='modify_goal'),
     url(r'^goal/(?P<goal_id>[0-9]+)/$', goal_views.detail_goal, name='detail'),
+    url(r'^goal/(?P<goal_id>[0-9]+)/modify$', goal_views.modify_goal, name='modify_goal'),
+    url(r'^goal/(?P<goal_id>[0-9]+)/new_sub_goal$', goal_views.new_sub_goal, name='new_sub_goal'),
     url(r'^admin/', admin.site.urls),
 ]
--- GoalSetTracker/goal/views.py
+++ GoalSetTracker/goal/views.py
@@ -28,9 +28,9 @@ def new_goal(request):
         return HttpResponseRedirect("/login")
 
 #TODO: Sacar el "default" de goal_id
-def new_sub_goal(request, goal_id=1):
+def new_sub_goal(request, goal_id):
+    goal = get_object_or_404(Goal, pk=goal_id)
     if request.user.is_authenticated:
-        goal = get_object_or_404(Goal, pk=goal_id)
         if request.method == "POST":
             finish_date = request.POST.get("finish_date")
             if finish_date == '':
@@ -38,7 +38,7 @@ def new_sub_goal(request, goal_id=1):
             goal.subgoal_set.create(sub_goal_text=request.POST.get("sub_goal_text"))
             return redirect_goal(goal_id)
         else:
-            return render(request, 'goal/new_sub_goal.html')
+            return render(request, 'goal/new_sub_goal.html', {'goal': goal})
     else:
         return HttpResponseRedirect("/login")
 
@@ -47,6 +47,25 @@ def detail_goal(request, goal_id):
     if request.user.is_authenticated:
         goal = get_object_or_404(Goal, pk=goal_id)
         return render(request, 'goal/detail.html', {'goal': goal})
+
+def modify_goal(request, goal_id):
+    goal = get_object_or_404(Goal, pk=goal_id)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if request.POST.get("finish_date"):
+                goal.finish_date = request.POST.get("finish_date")
+            if request.POST.get("goal_text"):
+                goal.goal_text = request.POST.get("goal_text")
+            if request.POST.get("create_date"):
+                goal.create_date = request.POST.get("create_date")
+            if request.POST.get("priority"):
+                goal.priority = request.POST.get("priority")
+            if request.POST.get("state"):
+                goal.state = request.POST.get("state")
+            goal.save()
+            return redirect_goal(goal_id)
+        else:
+            return render(request, 'goal/modify_goal.html', {'goal': goal})
 
 def redirect_goal(id):
     return HttpResponseRedirect("/goal/{}".format(id))
