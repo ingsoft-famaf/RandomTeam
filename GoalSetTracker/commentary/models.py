a04ad5d77f10f5786760a04a073c3f1a4d56ceae
lucianoinso <lucianoinso@gmail.com>
2016-10-25 20:44:08 -0300

    cambiadas varias funciones para que acepten goals y subgoals, creado nuevo template para subcomments, falta ver modificar y eliminar para que funcionen en comments y comments de subgoals

--- GoalSetTracker/commentary/models.py
+++ GoalSetTracker/commentary/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.contrib.auth.models import User  
-from goal.models import Goal
+from goal.models import AbstractGoal
 
 @python_2_unicode_compatible
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
-    goal = models.ForeignKey(Goal, on_delete = models.CASCADE)
+    goal = models.ForeignKey(AbstractGoal, on_delete = models.CASCADE)
     text = models.CharField(max_length=400)
     create_date = models.DateTimeField(auto_now_add=True)
 
