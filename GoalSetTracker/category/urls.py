daf9c4ac926795164a561d604e92b15958f0b29f
nicoolas@bitbuket.com <nicoolas>
2016-10-26 03:46:06 -0300

    categorias agregadas, faltan relacionar categoria goals

--- GoalSetTracker/category/urls.py
+++ GoalSetTracker/category/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.category, name='cat'),
+    url(r'^$', views.category, name='category'),
+    url(r'^(?P<category_id>[0-9]+)/$',views.category_info,name='category-inf'),
+    url(r'^(?P<category_id>[0-9]+)/edit/$',views.category_edit,name='category_edit'),
+    url(r'^category_new/$',views.category_new,name='category_new'),
+    
 ]
+
--- GoalSetTracker/category/views.py
+++ GoalSetTracker/category/views.py
@@ -4,17 +4,63 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from login.views import redirect_home
 from goal.models import Goal
+from .models import Category
+from django.template import loader
 
 def category(request):
     if request.user.is_authenticated:
         try:
             user = User.objects.get(username=request.user.username)
-        except Exception as e:
+            category_all = user.category_set.all()
+        except Exception as a:
             return HttpResponse("El usuario no existe")
-        return HttpResponse("estas en categorias")
+        context = {'category_all': category_all,}
+        return render(request,'category/category.html',context)
     else:
         return HttpResponseRedirect("/login")
+
+def category_info(request,category_id):
+    if request.user.is_authenticated:
+       catego = get_object_or_404(Category, pk=category_id)
+       return render(request,'category/category_info.html',{'catego' : catego})
+    else:
+        return HttpResponseRedirect("/login")
+
+def category_new(request):
+   if request.user.is_authenticated:
+        try:
+            user = User.objects.get(username=request.user.username)
+        except Exception as e:
+            return HttpResponse("El usuario no existe")
+<<<<<<< HEAD
+        return HttpResponse("estas en categorias")
+=======
+        if request.method == "POST":
+             user.category_set.create(categoria_tipo=request.POST.get("categoria_tipo"))
+             return redirect_home(request.user.username)
+        else:
+            return render(request,'category/category_new.html')
+   else:
+        return HttpResponseRedirect("/login")
+
+def category_edit(request,category_id):
+    if request.user.is_authenticated:
+        try:
+            user = User.objects.get(username=request.user.username)
+            catego = get_object_or_404(Category, pk=category_id)
+        except Exception as e:
+            return HttpResponse("El usuario no existe")
+        if request.method == "POST":
+             catego.categoria_tipo = categoria_tipo=request.POST.get("categoria_tipo")
+             return redirect_home(request.user.username)
+        else:
+            return render(request,'category/category_edit.html',{'catego' : catego})
+>>>>>>> categorias agregadas, faltan relacionar categoria goals
+    else:
+        return HttpResponseRedirect("/login")
+
+
+
 
