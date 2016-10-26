1f09e95524e0ada78d010939898f50eb63921707
nicoolas <nicoolas@bitbucket.org>
2016-10-26 10:31:31 -0300

    Congruencia de idioma en Category

--- GoalSetTracker/category/urls.py
+++ GoalSetTracker/category/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.category, name='category'),
-    url(r'^(?P<category_id>[0-9]+)/$',views.category_info,name='category-inf'),
+    url(r'^(?P<category_id>[0-9]+)/$',views.category_info,name='category_inf'),
     url(r'^(?P<category_id>[0-9]+)/edit/$',views.category_edit,name='category_edit'),
     url(r'^category_new/$',views.category_new,name='category_new'),
     
--- GoalSetTracker/category/views.py
+++ GoalSetTracker/category/views.py
@@ -15,7 +15,7 @@ def category(request):
             user = User.objects.get(username=request.user.username)
             category_all = user.category_set.all()
         except Exception as a:
-            return HttpResponse("El usuario no existe")
+            return HttpResponse("The user do not exist")
         context = {'category_all': category_all,}
         return render(request,'category/category.html',context)
     else:
@@ -23,8 +23,8 @@ def category(request):
 
 def category_info(request,category_id):
     if request.user.is_authenticated:
-       catego = get_object_or_404(Category, pk=category_id)
-       return render(request,'category/category_info.html',{'catego' : catego})
+       category = get_object_or_404(Category, pk=category_id)
+       return render(request,'category/category_info.html',{'category' : category})
     else:
         return HttpResponseRedirect("/login")
 
@@ -33,9 +33,9 @@ def category_new(request):
         try:
             user = User.objects.get(username=request.user.username)
         except Exception as e:
-            return HttpResponse("El usuario no existe")
+            return HttpResponse("The user do not exist")
         if request.method == "POST":
-             user.category_set.create(categoria_tipo=request.POST.get("categoria_tipo"))
+             user.category_set.create(category_tipo=request.POST.get("category_tipo"))
              return redirect_home(request.user.username)
         else:
             return render(request,'category/category_new.html')
@@ -46,14 +46,16 @@ def category_edit(request,category_id):
     if request.user.is_authenticated:
         try:
             user = User.objects.get(username=request.user.username)
-            catego = get_object_or_404(Category, pk=category_id)
+            category = get_object_or_404(Category, pk=category_id)
         except Exception as e:
-            return HttpResponse("El usuario no existe")
+            return HttpResponse("The user do not exist")
         if request.method == "POST":
-             catego.categoria_tipo = categoria_tipo=request.POST.get("categoria_tipo")
+             if request.POST.get("category_tipo"):
+                  category.category_tipo = request.POST.get("category_tipo")
+             category.save()
              return redirect_home(request.user.username)
         else:
-            return render(request,'category/category_edit.html',{'catego' : catego})
+            return render(request,'category/category_edit.html',{'category' : category})
     else:
         return HttpResponseRedirect("/login")
 
