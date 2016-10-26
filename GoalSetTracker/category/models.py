2286fb12154d227bd82a65f7ec99b17f2ea72587
nicoolas@bitbuket.com <nicoolas>
2016-10-26 03:46:06 -0300

    categorias agregadas, faltan relacionar categoria goals

--- GoalSetTracker/category/models.py
+++ GoalSetTracker/category/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from goal.models import Goal
 
@@ -9,3 +10,6 @@ from goal.models import Goal
 class Category(models.Model):
     categoria_tipo = models.CharField(max_length=200)
     categoria_relacion = models.ManyToManyField(Goal)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.categoria_tipo
--- GoalSetTracker/category/views.py
+++ GoalSetTracker/category/views.py
@@ -4,30 +4,59 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from login.views import redirect_home
 from goal.models import Goal
+from .models import Category
+from django.template import loader
 
 def category(request):
     if request.user.is_authenticated:
         try:
             user = User.objects.get(username=request.user.username)
-        except Exception as e:
+            category_all = user.category_set.all()
+        except Exception as a:
             return HttpResponse("El usuario no existe")
-        return HttpResponse("estas en categorias")   
-#        if request.method == "POST":
-#            finish_date = request.POST.get("finish_date")
-#            if finish_date == '':
-#                finish_date = timezone.now()
-#            user.goal_set.create(goal_text=request.POST.get("goal_text"),
-#                                 finish_date=finish_date,
-#                                 create_date=timezone.now(),
-#                                 priority = request.POST.get("priority"),
-#                                 state = request.POST.get("state")
-#                                 )
-#            return redirect_home(request.user.username)
-#        else:
-#            return render(request, 'goal/new_goal.html')
+        context = {'category_all': category_all,}
+        return render(request,'category/category.html',context)
     else:
         return HttpResponseRedirect("/login")
+
+def category_info(request,category_id):
+    if request.user.is_authenticated:
+       catego = get_object_or_404(Category, pk=category_id)
+       return render(request,'category/category_info.html',{'catego' : catego})
+    else:
+        return HttpResponseRedirect("/login")
+
+def category_new(request):
+   if request.user.is_authenticated:
+        try:
+            user = User.objects.get(username=request.user.username)
+        except Exception as e:
+            return HttpResponse("El usuario no existe")
+        if request.method == "POST":
+             user.category_set.create(categoria_tipo=request.POST.get("categoria_tipo"))
+             return redirect_home(request.user.username)
+        else:
+            return render(request,'category/category_new.html')
+   else:
+        return HttpResponseRedirect("/login")
+
+def category_edit(request,category_id):
+    if request.user.is_authenticated:
+        try:
+            user = User.objects.get(username=request.user.username)
+            catego = get_object_or_404(Category, pk=category_id)
+        except Exception as e:
+            return HttpResponse("El usuario no existe")
+        if request.method == "POST":
+             catego.categoria_tipo = categoria_tipo=request.POST.get("categoria_tipo")
+             return redirect_home(request.user.username)
+        else:
+            return render(request,'category/category_edit.html',{'catego' : catego})
+    else:
+        return HttpResponseRedirect("/login")
+
+
+
 
