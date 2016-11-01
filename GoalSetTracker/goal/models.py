7a4caa79c3b98839cccad2cdfd63c6e28547e4ba
Francisco Nievas <frannievas@gmail.com>
2016-10-31 21:03:11 -0300

    Se modifico que para ver si una meta esta vencida o no se utilice su estado y no su fecha. Cambiado el css para poder mostrar muchas metas en la pantalla

--- GoalSetTracker/goal/models.py
+++ GoalSetTracker/goal/models.py
@@ -31,7 +31,7 @@ class AbstractGoal(models.Model):
         return self.goal_text
 
     def is_active(self):
-        return self.finish_date > timezone.now()
+        return (self.state != "FINISH")
 
 @python_2_unicode_compatible
 class Goal(AbstractGoal):
